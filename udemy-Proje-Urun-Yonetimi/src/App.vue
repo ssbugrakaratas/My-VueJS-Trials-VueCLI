7cbb84f9abbd32f3f0aebd87d2183acd2f69d9a5
ssbugrakaratas <gssametbugra@gmail.com>
2022-05-23 13:35:42 +0300

    Proje Urun Yönetimine Baslangıc Inıtalization

---
+++ udemy-Proje-Urun-Yonetimi/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <div class="container text-center">
+      <br>
+      <h1>{{ title }}</h1>
+      <hr>
+    </div>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "app",
+  data() {
+    return {
+      title: "Bolum 19",
+
+    };
+  },
+
+}
+</script>
+
+<style>
+</style>
