a915145a6dbc4bbaa6bee300585eadeeb19e2e34
ssbugrakaratas <gssametbugra@gmail.com>
2022-05-23 09:41:17 +0300

    init

--- udemy-bolum-18-Vuex/src/App.vue
+++ udemy-bolum-18-Vuex/src/App.vue
@@ -6,29 +6,18 @@
       <hr>
     </div>
 
-    <router-view name="header-top"></router-view>
-    <router-view></router-view>
-    <router-view name="header-bottom"></router-view>
-
-
   </div>
 </template>
 
 <script>
-import Home from './components/Home.vue'
-import User from './components/user/User.vue';
-import UserDetail from './components/user/UserDetail.vue';
-import UserEdit from './components/user/UserEdit.vue';
-import UserStart from './components/user/UserStart.vue';
-import Header from './components/Header.vue';
+
 export default {
   name: "app",
   data() {
     return {
-      title: "Bolum 17",
+      title: "Bolum 18",
     };
   },
-  components: { Home, User, UserDetail, UserEdit, UserStart, Header }
 }
 </script>
 
