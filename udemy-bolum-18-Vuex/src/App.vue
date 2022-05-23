b3c0c9bb3786fb0e723782cf75a1a59f0e99911e
ssbugrakaratas <gssametbugra@gmail.com>
2022-05-23 09:52:27 +0300

    vuex install

--- udemy-bolum-18-Vuex/src/App.vue
+++ udemy-bolum-18-Vuex/src/App.vue
@@ -6,18 +6,28 @@
       <hr>
     </div>
 
+    <Result :counter="counter"></Result>
+    <hr>
+    <Counter @counterEvent="counter += $event"></Counter>
+
+
+
   </div>
 </template>
 
 <script>
+import Result from './components/Result.vue';
+import Counter from './components/Counter.vue';
 
 export default {
   name: "app",
   data() {
     return {
       title: "Bolum 18",
+      counter: 0,
     };
   },
+  components: { Result, Counter }
 }
 </script>
 
