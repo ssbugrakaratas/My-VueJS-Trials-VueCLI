725af70645833df959f6ee2ca655109bc53000d6
ssbugrakaratas <gssametbugra@gmail.com>
2022-05-24 17:46:14 +0300

    component kullanımına geçişten önce app vue üzerinde projenin taslağının yapılması

--- udemy-Proje-Tag-Manager/src/App.vue
+++ udemy-Proje-Tag-Manager/src/App.vue
@@ -7,18 +7,14 @@
     </div>
 
     <div class="tag-container">
-      <span class="tag">
-        <span class="tag-content">Vue</span>
-        <span class="tag-close">x</span>
+      <span class="tag" v-for="(tag, index) in tags">
+        <span class="tag-content">{{ tag }}</span>
+        <span class="tag-close" @click="deleteTagWithIndex(index)">x</span>
       </span>
-      <span class="tag">
-        <span class="tag-content">Vue</span>
-        <span class="tag-close">x</span>
-      </span>
-      <span class="tag">
-        <span class="tag-content">Vue</span>
-        <span class="tag-close">x</span>
-      </span>
+
+      <input type="text" @keyup.enter="addTag" @keyup.backspace="deleteTag">
+      <div class="error" v-if="error == 'bulundu'">Bu tag zaten vardır!!!</div>
+      <div class="error" v-else-if="error == 'bos'">Bos tag giremezsin</div>
     </div>
   </div>
 </template>
@@ -28,9 +24,50 @@ export default {
   name: 'app',
   data() {
     return {
-      title: 'Bolum-21'
+      title: 'Bolum-21',
+      tags: ["tag1", "asda"],
+      error: ""
     }
-  }
+  },
+  methods: {
+    addTag(event) {
+      let tagInput = event.target.value
+      let matchedTag = false
+
+      if (tagInput != null && tagInput.length > 0) {
+        this.tags.forEach(tag => {
+          if (tag.toLowerCase() === tagInput.toLowerCase()) {
+            matchedTag = true
+          }
+        });
+
+        if (!matchedTag) {
+          this.tags.push(tagInput)
+          event.target.value = null
+        } else {
+          this.error = 'bulundu'
+          setTimeout(() => {
+            this.error = ""
+          }, 2000);
+        }
+      } else {
+        this.error = 'bos'
+        setTimeout(() => {
+          this.error = ""
+        }, 2000);
+      }
+
+
+    },
+    deleteTag(event) {
+      if (event.target.value.length <= 0) {
+        this.tags.splice(this.tags.length - 1)
+      }
+    },
+    deleteTagWithIndex(index) {
+      this.tags.splice(index, 1)
+    }
+  },
 }
 </script>
 
@@ -46,10 +83,23 @@ div.tag-container {
   color: #000;
   cursor: default;
   font-size: 14px;
+  margin-right: 10px;
 }
 
 .tag .tag-close {
   font-size: 12px;
   cursor: pointer;
+}
+
+input {
+  outline: none;
+  height: 30px;
+  width: 100px;
+}
+
+.error {
+  font-size: 12px;
+  color: red;
+  margin-top: 10px;
 }
 </style>
