12d5ea5b44e404aae6dc0d3ffb1d7139c0abfd4f
ssbugrakaratas <gssametbugra@gmail.com>
2022-05-25 16:25:17 +0300

    init

---
+++ udemy-bolum-25-authentication/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="#app">
+    <div class="container text-center">
+      <br>
+      <h1>{{ title }}</h1>
+      <hr>
+    </div>
+    <Header></Header>
+    <HomePage></HomePage>
+  </div>
+</template>
+<script>
+
+import Header from "./components/Header";
+import Auth from "./pages/auth/Auth"
+import Homepage from "./pages/Homepage"
+import About from "./pages/About"
+
+export default {
+  data() {
+    return {
+      title: "Bolum-25"
+    }
+  },
+  components: {
+    Header,
+    Auth,
+    Homepage,
+    About
+  },
+}
+</script>
